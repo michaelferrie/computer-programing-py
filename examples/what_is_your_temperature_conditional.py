98fd6778f397aefbce01815c2cb1681db9a18517
Michael Ferrie <me@michaelferrie.com>
2020-10-27 19:42:00 +0000

    Create what_is_your_temperature_conditional.py

---
+++ examples/what_is_your_temperature_conditional.py
@@ -0,0 +1,8 @@
+# takes an input temperature and returns a message based on how warm it is 
+
+temperature = float(input("What is the temperature?: "))
+
+if temperature > 25:
+    print ("I dont't think you need a coat")
+else:
+    print ("Wear a coat its cold!!!")
