89a6bb2806138d6f4a6d399cce1e71b70cec6047
Michael Ferrie <me@michaelferrie.com>
2020-11-05 21:58:52 +0000

    Create for_and_while_examples.py

---
+++ examples/for_and_while_examples.py
@@ -0,0 +1,14 @@
+my_string = "hello there"
+
+for item in my_string:
+    if item == "e":
+       print ("This is an item: ", item)
+
+
+count = 0
+
+while count < 100:
+    count += 1
+    print ("This must be less 100: ", count)
+else:
+    print ("Must be more than 100", count)
