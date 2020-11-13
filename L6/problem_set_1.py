91f9a10abdbe81b66063f8e72abed8b80d680bb7
Michael Ferrie <me@michaelferrie.com>
2020-11-13 19:00:07 +0000

    Add files via upload

---
+++ L6/problem_set_1.py
@@ -0,0 +1,54 @@
+# Python Problem Set 1
+#
+# Student Name      - Firstname SecondName
+# Student Number    - ECxxxxxx
+# Date              - DD/MM/2020
+#
+'''
+Save this python script in your code folder as problem_set_1.py
+then run it, and edit the file, once complete upload to the Moodle.
+'''
+print ("******************************************")
+print ("******************************************")
+print ("printing out letters problem set 1")
+
+
+# Q1 Add the python code here to print out the string "Hello World":
+# your code below this line:
+
+
+
+# Q2 Using asterisks *** can you print out the first letter of your name
+# make it five or 6 lines high
+# your code below this line here is an example I:
+
+print ("***")
+print (" * ")
+print (" * ")
+print (" * ")
+print (" * ")
+print ("* *")
+# Q3 We can take user input using the input key word, can you edit the program below to say
+# Good Morning to the person who inputs their name
+user_name = input('Enter your name: ')
+print('Hello', user_name, '!')
+
+
+# Q4 Modify the program from Q3 - make it ask for the users name and then say back
+# - I know you like python - then the users name
+
+
+# Q5 we can use Python as a simple calculator
+number1 = 1
+number2 = 2
+number3 = number1 + number2
+# Write a program that prints out the value of number 3
+# your code below this line
+
+
+# Q6 Combine the code in the previous questions create a program that asks the
+# user to enter 2 numbers, the program should then add the numbers together
+# and print out the result
+# add your code below this line
+
+
