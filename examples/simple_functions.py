3f4b3144fd0bf0978e23e7ca614467f74d52583a
Michael Ferrie <me@michaelferrie.com>
2020-11-13 13:03:03 +0000

    Create simple_functions.py

---
+++ examples/simple_functions.py
@@ -0,0 +1,11 @@
+# simple adding function
+def two_num_multiplier(num_one,num_two):
+	return num_one * num_two
+
+print two_num_multiplier(3,10)
+
+# simple multiplying function
+def my_adding_function(x,y):
+	return x + y
+	
+print (my_adding_function(4,4))
