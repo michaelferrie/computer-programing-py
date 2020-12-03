ba7b2020328777e5b16be6ba93d74c5f246e8511
Michael Ferrie <me@michaelferrie.com>
2020-12-03 10:29:13 +0000

    Add files via upload

---
+++ NPA/for_example.py
@@ -0,0 +1,11 @@
+# Simple for loop example
+
+
+
+my_string = "why is the cinema closed"
+
+special_letters = "ea"
+
+for characters in my_string:
+    if characters in special_letters:
+        print ("I found one of the special characters" , characters)
