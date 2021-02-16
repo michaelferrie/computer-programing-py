a38aec4d2108159613bb16b087fc1285f2da89bb
Michael Ferrie <me@michaelferrie.com>
2021-02-16 20:57:23 +0000

    Create string_indexing.py

---
+++ examples/string_indexing.py
@@ -0,0 +1,25 @@
+#
+# Print Operations Slicing / Indexing Strings
+#
+
+my_string = "Edinburgh Castle on the Royal Mile"
+
+print (my_string) # Prints whole string
+
+print (my_string[0]) # Prints first char
+
+print (my_string[1:3]) # Prints 2 and 3
+
+print (my_string[:3]) # Gives first 3 values
+
+print (my_string[-1]) # Gives last char
+
+print (my_string[2:5]) # Slices 3,4 and 5 char
+
+print (my_string[1:8:3]) # Stride of 3
+
+print(len(my_string)) # Gives string length
+
+print(my_string.count("i")) # Counts instances of x
+
+print(my_string.find("a")) # Finds instances of x
